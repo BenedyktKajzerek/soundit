6c5da57094686ac84cffcc1a1cfef4bf798c9296
Benedykt <kajzerek.benedykt@gmail.com>
2024-02-25 18:47:47 +0100

    light startup

--- soundit/views.py
+++ soundit/views.py
@@ -1,6 +1,32 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+# from .models import ...
+
+
 def index(request):
     return HttpResponse("Hi, mom!")
+
+
+def login(request):
+    if request.method == "POST":
+        return render(request, "soundit/login.html")
+    
+    else:
+        return render(request, "soundit/login.html")
+
+
+def logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        return render(request, "soundit/register.html")
+
+    else:
+        return render(request, "soundit/register.html")
