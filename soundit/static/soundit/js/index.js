309492207e89c6bce8c90b7105dac0352688c840
Benedykt <kajzerek.benedykt@gmail.com>
2024-03-07 22:17:59 +0100

    section 6, reviews

---
+++ soundit/static/soundit/js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Clone music services twice so it slides smoothly 
+    const copy = document.querySelector('.logos-track').cloneNode(true);
+    document.querySelector('.logos-div').appendChild(copy);
+    document.querySelector('.logos-div').appendChild(copy.cloneNode(true));
+
+    // Available conversions
+    for (let i = 0; i < 5; i++) {
+        const copy2 = document.querySelector('.service-link').cloneNode(true);
+        document.querySelector('.services-div').appendChild(copy2);
+    }
+
+});
--- soundit/static/soundit/js/script.js
+++ soundit/static/soundit/js/script.js
@@ -4,15 +4,4 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('.nav').classList.toggle('display-block');
     });
 
-    // Clone music services twice so it slides smoothly 
-    const copy = document.querySelector('.logos-track').cloneNode(true);
-    document.querySelector('.logos-div').appendChild(copy);
-    document.querySelector('.logos-div').appendChild(copy.cloneNode(true));
-
-    // Available conversions
-    for (let i = 0; i < 5; i++) {
-        const copy2 = document.querySelector('.service-link').cloneNode(true);
-        document.querySelector('.services-div').appendChild(copy2);
-    }
-
 });
