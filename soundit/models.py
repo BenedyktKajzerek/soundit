97d7d2df381a4acbb357bb90e801a8a1cff143d8
Benedykt <kajzerek.benedykt@gmail.com>
2024-07-11 21:34:10 +0200

    minor fixes

--- soundit/models.py
+++ soundit/models.py
@@ -19,20 +19,20 @@ class User(AbstractUser):
     
 class SpotifyToken(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="spotify")
-    access_token = models.CharField(max_length=255)
-    token_type = models.CharField(max_length=50)
-    expires_in = models.DateTimeField()
-    refresh_token = models.CharField(max_length=255)
+    access_token = models.CharField(null=True, max_length=255)
+    token_type = models.CharField(null=True, max_length=50)
+    expires_in = models.DateTimeField(null=True)
+    refresh_token = models.CharField(null=True, max_length=255)
 
     def __str__(self):
         return f'{self.user}'
 
 class YouTubeToken(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="youtube")
-    access_token = models.CharField(max_length=255)
-    token_type = models.CharField(max_length=50)
-    expires_in = models.DateTimeField()
-    refresh_token = models.CharField(max_length=255, null=True, blank=True)
+    access_token = models.CharField(null=True, max_length=255)
+    token_type = models.CharField(null=True, max_length=50)
+    expires_in = models.DateTimeField(null=True)
+    refresh_token = models.CharField(null=True, max_length=255, blank=True)
 
     def __str__(self):
         return f'{self.user}'
